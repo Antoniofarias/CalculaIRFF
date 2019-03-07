8b415f44103dbb4728a39bb8bc757c8f51225d00
Antonio <antoniofjj@hotmail.com>
2019-03-07 15:48:40 -0300

    Atualização

--- CalculaIRFF/Program.cs
+++ CalculaIRFF/Program.cs
@@ -12,22 +12,70 @@ namespace CalculaSalarioLiquido
         {
             double baseCalculoIrff;
             double salarioLiquido;
-
+            double salarioBruto = 0;
+            double pensao = 0;
+            double qntDependente;
+            double dependente = 0;
+            double outrosDesc = 0;
             double valorVt;
             const double aliqVt = 0.06;
 
             Console.WriteLine("Digite o valor de seu salário");
-            double salarioBruto = double.Parse(Console.ReadLine());
+            #region Try Catch salarioBruto
+            try
+            {
+                salarioBruto = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Digite apenas caracteres numericos: " + e.Message);
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            #endregion
 
             Console.WriteLine("Digite o valor da pensão");
-            double pensao = Convert.ToDouble(Console.ReadLine());
+            #region Try Catch pensao
+            try
+            {
+                pensao = Convert.ToDouble(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Digite apenas caracteres numericos: " + e.Message);
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            #endregion
 
             Console.WriteLine("Digite quantidade de dependentes");
-            double qntDependente = double.Parse(Console.ReadLine());
-            double dependente = qntDependente * 189.59;
+            #region Try Catch dependente
+            try
+            {
+                qntDependente = double.Parse(Console.ReadLine());
+                dependente = qntDependente * 189.59;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Digite apenas caracteres numericos: " + e.Message);
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            #endregion
 
             Console.WriteLine("Digite o valor de outros descontos");
-            double outrosDesc = double.Parse(Console.ReadLine());
+            #region Try Catch outros Descontos
+            try
+            {
+                outrosDesc = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Digite apenas caracteres numericos: " + e.Message);
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            #endregion
 
             CalculaINSS inss = new CalculaINSS();
             CalculaIRFF irff = new CalculaIRFF();
@@ -38,6 +86,7 @@ namespace CalculaSalarioLiquido
 
             baseCalculoIrff = salarioBruto - inss.contribuicaoInss - dependente - pensao;
 
+            #region IF
             if (baseCalculoIrff <= irff.valorIrff1)
             {
                 baseCalculoIrff = (baseCalculoIrff * irff.aliqIrff1) - irff.deducaoIrff1;
@@ -54,12 +103,13 @@ namespace CalculaSalarioLiquido
             {
                 baseCalculoIrff = (baseCalculoIrff * irff.aliqIrff4) - irff.deducaoIrff4;
             }
-            else if(baseCalculoIrff > irff.valorIrff8)
+            else if (baseCalculoIrff > irff.valorIrff8)
             {
                 baseCalculoIrff = (baseCalculoIrff * irff.aliqIrff5) - irff.deducaoIrff5;
             }
-                            
-                salarioLiquido = salarioBruto - baseCalculoIrff - inss.contribuicaoInss - valorVt - outrosDesc;
+            #endregion
+
+            salarioLiquido = salarioBruto - baseCalculoIrff - inss.contribuicaoInss - valorVt - outrosDesc;
 
             Console.Clear();
             Console.WriteLine("Desconto de pensão: " + pensao);
